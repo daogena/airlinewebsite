4d1b629a6eae37e3d260dee26d686c65d05bf08e
Gena <daogena@gmail.com>
2020-12-08 21:12:58 -0600

    commit

--- server/db.js
+++ server/db.js
@@ -1,9 +1,14 @@
 const Pool = require('pg').Pool;
+const fs = require('fs'); 
+
+let data = fs.readFileSync('password.txt', 'utf8');
+let user = data.toString().substring(0,8); 
+let password = data.toString().substring(9,18);
 
 const pool = new Pool({
   host: 'code.cs.uh.edu',
-  user: 'cosc0137',
-  password: '1845342DG',
+  user: user,
+  password: password,
   port: 5432,
   database: 'COSC3380'
 });
